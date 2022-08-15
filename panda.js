27806a820450acd7357124e27779b1fa4fa5fc56
AsiburSumon <sumonasif68@gmail.com>
2022-08-15 11:10:54 +0600

    commit

--- panda.js
+++ panda.js
@@ -1,2 +1,11 @@
-const myStyle =document.querySelectorAll('.container h2');
-myStyle.style.color = 'lightblue';
+const myStyle = document.querySelectorAll("h2");
+for(let i=0; i<myStyle.length; i++){
+    myStyle[i].style.color="lightblue";
+}
+
+document.getElementById('backpack').style.backgroundColor = "tomato";
+
+const cardStyle = document.querySelectorAll(".card");
+for(let i=0; i<cardStyle.length; i++){
+    cardStyle[i].style.borderRadius="30px";
+}
